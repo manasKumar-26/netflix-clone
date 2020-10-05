0c9a567d442dc5377fc44e937450f1a9f59148ed
manaskumar26 <67231881+manaskumar26@users.noreply.github.com>
2020-10-05 20:19:29 +0530

    Netflix First Commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Building Netflix-Clone Whoooo</h1>
+    </div>
+  );
+}
+
+export default App;
